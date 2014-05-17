b23e65aaf15639ce27aa116e18ee27fea742fbfa
Condone <dilmher@gmail.com>
2014-05-17 15:45:20 +0200

    Add icon

--- osuTrainer/Forms/OsuTrainer.Designer.cs
+++ osuTrainer/Forms/OsuTrainer.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OsuTrainer));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -282,6 +283,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "OsuTrainer";
             this.Text = "osu! Trainer";
