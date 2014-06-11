d3bb4f362175e4a30b5e33aa4f7b4ed7c64569d6
Condone <dilmher@gmail.com>
2014-06-11 11:42:45 +0200

    Check if another program instance is already opened

--- osuTrainer/App.xaml.cs
+++ osuTrainer/App.xaml.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows;
 
 namespace osuTrainer
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _instanceMutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createdNew;
+            _instanceMutex = new Mutex(true, @"osuTrainer", out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("osu! Trainer is already running.");
+                _instanceMutex = null;
+                Application.Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_instanceMutex != null)
+                _instanceMutex.ReleaseMutex();
+            base.OnExit(e);
+        }
     }
 }
